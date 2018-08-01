083d77d9e56e3bf5fda3febf61f2157911537b95
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-08-01 14:14:49 +0900

    API経由で記事の取得完了

--- Qiita-prac/ArticleListViewController.swift
+++ Qiita-prac/ArticleListViewController.swift
@@ -7,29 +7,29 @@
 //
 
 import UIKit
+import Alamofire
 
 class ArticleListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  
+  
+  @IBOutlet weak var articleTableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    getArticles()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  func getArticles(){
+    // APIへリクエストを送信
+    Alamofire.request("https://qiita.com/api/v2/items", method: .get)
+      // responseJSONメソッドは1つの関数を引数(Response<AnyObject, NSError>型 )に取る
+      .responseJSON { response in
+        print(response.result.value)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
 }
