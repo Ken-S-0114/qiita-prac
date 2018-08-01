1deae254831f2f59c0ac460580896f80429c42e0
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-08-01 15:43:41 +0900

    完成

--- Qiita-prac/ArticleListViewController.swift
+++ Qiita-prac/ArticleListViewController.swift
@@ -12,19 +12,18 @@ import SwiftyJSON
 
 class ArticleListViewController: UIViewController {
   
-  
   @IBOutlet weak var articleTableView: UITableView!
+  
+  // 記事を入れるプロパティを定義
+  var articles: [[String: String?]] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
     getArticles()
+
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-  
-  func getArticles(){
+  private func getArticles(){
     // APIへリクエストを送信
     Alamofire.request("https://qiita.com/api/v2/items", method: .get)
       // responseJSONメソッドは1つの関数を引数(Response<AnyObject, NSError>型 )に取る
@@ -34,12 +33,29 @@ class ArticleListViewController: UIViewController {
         let json = JSON(object)
         // タプル型の引数にはその要素が何番目かと記事1つのデータの2つの要素が入る
         json.forEach { (_, json) in
-          // 記事タイトルを表示
-          print(json["title"].string)
-          // 投稿者のユーザーIDを表示
-          print(json["user"]["id"].string)
+          let article: [String: String?] = [
+            "title": json["title"].string,
+            "userId": json["user"]["id"].string
+          ]
+          self.articles.append(article)
         }
+        self.articleTableView.reloadData()
     }
   }
   
+}
+
+extension ArticleListViewController: UITableViewDataSource {
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return articles.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath)
+    let article = articles[indexPath.row]
+    cell.textLabel?.text = article["title"]!
+    cell.detailTextLabel?.text = article["userId"]!
+    return cell
+  }
 }
